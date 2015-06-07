ca2ff4c13324b6adf2a2aaf9d522cac97d99c4e9
rlfs@cin.ufpe.br <salqop21>
2015-06-07 15:51:29 -0500

    3 changes

--- src/test/ClientA.java
+++ src/test/ClientA.java
@@ -12,11 +12,11 @@ public class ClientA {
 			if (args.length == 0) socket = SSocket.getNewSSocket(args[0] = "localhost", 5999);
 			else socket = SSocket.getNewSSocket(args[0], 5999);
 			socket.listen(5998);
-			System.out.println(socket.readString());
-			System.out.println(socket.readInt());
+			System.out.println("Data received: " + socket.readString());
+			System.out.println("Data received: " +socket.readInt());
 			socket.sendString("Sending file");
 			socket.sendFile("Rubens SCSA.txt");
-			System.out.println(socket.readString());
+			System.out.println("Data received: " +socket.readString());
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
